4ba650008e25a7b5181a84de759e5849dca7d41c
mirangee <pmrang@live.co.kr>
2024-05-09 14:58:37 +0900

    할 일 추가, 할 일 목록 렌더링

--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -3,21 +3,44 @@ import '../scss/TodoInput.scss';
 import { MdAdd } from 'react-icons/md';
 import cn from 'classnames';
 
-const TodoInput = () => {
+const TodoInput = ({ addTodo }) => {
   // 입력창이 열리는 여부를 표현하는 상태값
   const [open, setOpen] = useState(false);
+
+  // 할 일 입력창에 입력한 내용을 표현하는 상태값
+  const [todoText, setTodoText] = useState('');
 
   // + 버튼 클릭 시 이벤트
   const onToggle = () => {
     setOpen(!open);
   };
 
+  // input 태그의 이벤트 핸들러
+  const todoChangeHandler = (e) => {
+    setTodoText(e.target.value);
+  };
+
+  // submit 이벤트 핸들러
+  const submitHandler = (e) => {
+    e.preventDefault(); //태그의 기본 기능 제한(submit 막기)
+
+    // 부모 컴포넌트가 전달한 함수의 매개값으로 입력값 넘기기
+    addTodo(todoText);
+
+    setTodoText(''); //입력이 끝나면 입력창 비우기
+  };
+
   return (
     <>
       {open && (
         <div className="form-wrapper">
-          <form className="insert-form">
-            <input type="text" placeholder="할 일 입력 후 엔터를 누르세요!" />
+          <form className="insert-form" onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="할 일 입력 후 엔터를 누르세요!"
+              onChange={todoChangeHandler}
+              value={todoText}
+            />
           </form>
         </div>
       )}
