99c11de380c7e3624eb608fe51f3828d6ead821e
mirangee <pmrang@live.co.kr>
2024-05-20 12:49:07 +0900

    join.js 리팩토링(fReducer를 활용한 상태 관리)

--- src/Components/user/Join.js
+++ src/Components/user/Join.js
@@ -6,62 +6,22 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { API_BASE_URL, USER } from '../../Config/host-config';
+import { initialState, joinReducer } from './joinReducer';
+import { debounce } from 'lodash';
 
 const Join = () => {
-  // 상태 변수로 회원가입 입력값 관리
-  const [userValue, setUserValue] = useState({
-    userName: '',
-    password: '',
-    email: '',
-  });
+  // useReducer를 사용해서 리듀서 함수 등록, state와 dispatch 함수를 전달 받음
+  const [state, dispatch] = useReducer(joinReducer, initialState);
 
-  // 검증 메세지에 대한 상태변수 관리
-  // 입력값과 메세지는 따로 상태 관리(메세지는 백엔드로 보내줄 필요 없음)
-  // 메세지 영역은 각 입력창마다 존재(이름, 이메일, 비밀번호...)하기 때문에 객체 형태로 한 번에 관리.
-  const [message, setMessage] = useState({
-    userName: '',
-    password: '',
-    passwordCheck: '',
-    email: '',
-  });
-
-  // 검증 완료 체크에 대한 상태변수 관리
-  // 각각의 입력창마다 유효성 검증 상태를 관리해야 하기 때문에 객체로 선언.
-  // 상태를 유지하려는 이유 -> 스타일, 마지막에 회원가입 버튼 누를 때 까지 검증 상태를 유지해야 하기 때문.
-  const [correct, setCorrect] = useState({
-    userName: false,
-    password: false,
-    passwordCheck: false,
-    email: false,
-  });
-
-  // 검증된 데이터를 각각의 상태변수에 저장해 주는 함수.
-  const saveInputState = ({ key, inputValue, flag, msg }) => {
-    // 입력값 세팅
-    // 패스워드 확인 입력값은 굳이 userValue 상태로 유지할 필요가 없기 때문에
-    // 임의의 문자열 'pass'를 넘기고 있습니다. -> pass가 넘어온다면 setUserValue()를 실행하지 않겠다.
-    inputValue !== 'pass' &&
-      setUserValue((oldVal) => {
-        return { ...oldVal, [key]: inputValue };
-      });
-
-    // 메세지 세팅
-    setMessage((oldMsg) => {
-      return { ...oldMsg, [key]: msg }; // key 변수의 값을 프로퍼티 키로 활용하는 중.
-    });
-
-    // 입력값 검증 상태 세팅
-    setCorrect((oldCorrect) => {
-      return { ...oldCorrect, [key]: flag };
-    });
-  };
+  // 상태 객체에서 각각의 상태 객체값을 분해 할당
+  const { userValue, message, correct } = state;
 
   // 이름 입력창 체인지 이벤트 핸들러
-  const nameHandler = (e) => {
+  const nameHandler = debounce((inputValue) => {
+    console.log('nameHandler가 동작함!');
     const nameRegex = /^[가-힣]{2,5}$/;
-    const inputValue = e.target.value;
 
     // 입력값 검증
     let msg; // 검증 메세지를 저장할 변수
@@ -76,14 +36,22 @@ const Join = () => {
       flag = true;
     }
 
-    // saveInputState에게 이 핸들러에서 처리한 여러가지 값을 객체로 한번에 넘기기.
-    saveInputState({
+    dispatch({
+      type: 'SET_USER_VALUE',
       key: 'userName',
-      inputValue,
-      msg,
-      flag,
+      value: inputValue,
     });
-  };
+    dispatch({
+      type: 'SET_MESSAGE',
+      key: 'userName',
+      value: msg,
+    });
+    dispatch({
+      type: 'SET_CORRECT',
+      key: 'userName',
+      value: flag,
+    });
+  }, 500);
 
   // 이메일 중복 체크 서버 통신 함수
   const fetchDuplicateCheck = (email) => {
@@ -100,19 +68,27 @@ const Join = () => {
           msg = '사용 가능한 이메일 입니다.';
           flag = true;
         }
-        // 중복 확인 후 상태값 변경.
-        saveInputState({
+
+        dispatch({
+          type: 'SET_USER_VALUE',
           key: 'email',
-          inputValue: email,
-          msg,
-          flag,
+          value: email,
+        });
+        dispatch({
+          type: 'SET_MESSAGE',
+          key: 'email',
+          value: msg,
+        });
+        dispatch({
+          type: 'SET_CORRECT',
+          key: 'email',
+          value: flag,
         });
       });
   };
 
   // 이메일 입력창 체인지 이벤트 핸들러
-  const emailHandler = (e) => {
-    const inputValue = e.target.value;
+  const emailHandler = debounce((inputValue) => {
     const emailRegex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
 
     let msg;
@@ -126,26 +102,40 @@ const Join = () => {
       // 이메일 중복 체크
       fetchDuplicateCheck(inputValue);
     }
-
-    // 중복확인 후에만 상태변경 하는 것이 아닙니다!
-    // 입력창이 비거나, 정규표현식 위반인 경우에도 상태는 변경 되어야 합니다.
-    saveInputState({
+    dispatch({
+      type: 'SET_USER_VALUE',
       key: 'email',
-      inputValue,
-      msg,
-      flag,
+      value: inputValue,
     });
-  };
+    dispatch({
+      type: 'SET_MESSAGE',
+      key: 'email',
+      value: msg,
+    });
+    dispatch({
+      type: 'SET_CORRECT',
+      key: 'email',
+      value: flag,
+    });
+  }, 500);
 
   // 패스워드 입력창 체인지 이벤트 핸들러
-  const passwordHandler = (e) => {
+  const passwordHandler = debounce((inputValue) => {
     // 패스워드가 변경됐다? -> 패스워드 확인란도 초기화 시킨다.
     document.getElementById('password-check').value = '';
 
-    setMessage({ ...message, passwordCheck: '' });
-    setCorrect({ ...correct, passwordCheck: false });
+    dispatch({
+      type: 'SET_MESSAGE',
+      key: 'passwordCheck',
+      value: '',
+    });
 
-    const inputValue = e.target.value;
+    dispatch({
+      type: 'SET_CORRECT',
+      key: 'passwordCheck',
+      value: false,
+    });
+
     const pwRegex =
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/;
 
@@ -157,37 +147,50 @@ const Join = () => {
     } else if (!pwRegex.test(inputValue)) {
       msg = '8글자 이상의 영문, 숫자, 특수문자를 포함해 주세요.';
     } else {
-      msg = '사용 가능한 비밀번호 입니다.';
+      msg = '사용 가능한 비밀번호입니다.';
       flag = true;
     }
 
-    saveInputState({
+    dispatch({
+      type: 'SET_USER_VALUE',
       key: 'password',
-      inputValue,
-      msg,
-      flag,
+      value: inputValue,
     });
-  };
+    dispatch({
+      type: 'SET_MESSAGE',
+      key: 'password',
+      value: msg,
+    });
+    dispatch({
+      type: 'SET_CORRECT',
+      key: 'password',
+      value: flag,
+    });
+  }, 500);
 
   // 비밀번호 확인란 체인지 이벤트 핸들러
-  const pwCheckHandler = (e) => {
+  const pwCheckHandler = debounce((inputValue) => {
     let msg;
     let flag = false;
-    if (!e.target.value) {
+    if (!inputValue) {
       msg = '비밀번호 확인란은 필수입니다.';
-    } else if (userValue.password !== e.target.value) {
+    } else if (userValue.password !== inputValue) {
       msg = '비밀번호가 일치하지 않습니다.';
     } else {
       msg = '비밀번호가 일치합니다.';
       flag = true;
     }
-    saveInputState({
+    dispatch({
+      type: 'SET_MESSAGE',
       key: 'passwordCheck',
-      inputValue: 'pass',
-      msg,
-      flag,
+      value: msg,
     });
-  };
+    dispatch({
+      type: 'SET_CORRECT',
+      key: 'passwordCheck',
+      value: flag,
+    });
+  }, 500);
 
   // 4개의 입력창이 모두 검증에 통과했는지 여부를 검사
   const isValid = () => {
@@ -246,7 +249,7 @@ const Join = () => {
               id="username"
               label="유저 이름"
               autoFocus
-              onChange={nameHandler}
+              onChange={(e) => nameHandler(e.target.value)}
             />
             <span
               style={correct.userName ? { color: 'green' } : { color: 'red' }}
@@ -263,7 +266,7 @@ const Join = () => {
               label="이메일 주소"
               name="email"
               autoComplete="email"
-              onChange={emailHandler}
+              onChange={(e) => emailHandler(e.target.value)}
             />
             <span style={correct.email ? { color: 'green' } : { color: 'red' }}>
               {message.email}
@@ -279,7 +282,7 @@ const Join = () => {
               type="password"
               id="password"
               autoComplete="current-password"
-              onChange={passwordHandler}
+              onChange={(e) => passwordHandler(e.target.value)}
             />
             <span
               style={correct.password ? { color: 'green' } : { color: 'red' }}
@@ -298,7 +301,7 @@ const Join = () => {
               type="password"
               id="password-check"
               autoComplete="check-password"
-              onChange={pwCheckHandler}
+              onChange={(e) => pwCheckHandler(e.target.value)}
             />
             <span
               id="check-span"
--- src/Components/user/joinReducer.js
+++ src/Components/user/joinReducer.js
@@ -19,7 +19,11 @@ export const initialState = {
 };
 
 // 회원 가입 쪽에서 여러 상태값을 중앙 집중화해서 처리할 리듀서 함수
-export default reducer = (state, action) => {
+// state: 최신의 상태값이 전달
+// action.type: 어떠한 상태값을 변경하는지에 대한 값
+// action.key: 입력창이 어디인지
+// action.value: 입력값이 무엇인지
+export const joinReducer = (state, action) => {
   switch (action.type) {
     case 'SET_USER_VALUE':
       return {
@@ -31,10 +35,24 @@ export default reducer = (state, action) => {
       };
 
     case 'SET_MESSAGE':
+      return {
+        ...state,
+        message: {
+          ...state.message,
+          [action.key]: action.value,
+        },
+      };
 
     case 'SET_CORRECT':
+      return {
+        ...state,
+        correct: {
+          ...state.correct,
+          [action.key]: action.value,
+        },
+      };
 
     default:
-      break;
+      return state;
   }
 };
